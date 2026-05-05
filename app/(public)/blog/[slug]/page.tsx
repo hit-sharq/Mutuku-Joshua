5004c8dba986380e027bc8f4d5c9d06fe95bf111
Joshua Mwendwa <leeroyjoshu087@gmail.com>
2026-05-05 16:46:09 +0300

    .

--- app/(public)/blog/[slug]/page.tsx
+++ app/(public)/blog/[slug]/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link"
 import { notFound } from "next/navigation"
 import { prisma } from "@/lib/prisma"
 
+export const dynamic = "force-dynamic"
+
 type BlogPostType = {
   id: string
   title: string
--- app/(public)/blog/page.tsx
+++ app/(public)/blog/page.tsx
@@ -22,6 +22,8 @@ async function getBlogPosts(): Promise<BlogPostType[]> {
   })
 }
 
+export const dynamic = "force-dynamic"
+
 export default async function BlogPage() {
   const posts = await getBlogPosts()
 
